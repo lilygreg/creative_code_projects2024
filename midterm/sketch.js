514952f827d65170b1875ddd76fdc66fca4844b7
lilygreg <lilygreg334@gmail.com>
2024-10-02 11:02:56 -0500

    trying

--- midterm/sketch.js
+++ midterm/sketch.js
@@ -1,7 +1,21 @@
-function setup() {
-    createCanvas(windowWidth,windowHeight);
-  }
+let defaultface;
+let nails;
+let polish;
 
-  function draw() {
-   
-  }
+function preload(){
+  defaultface = loadImage("midterm_defaultface.png");
+  nails = loadImage("withnails.png");
+ polish = loadImage("justnailpolish.png");
+  
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+//  background(220);
+  image(defaultface,width/2,height/2,defaultface.width,defaultface.height);
+  
+  
+}
