2dfcfb03a90f9a388cea74f51899fd555f52b8f0
lilygreg <lilygreg334@gmail.com>
2024-10-30 12:57:34 -0500

    play

--- p5play/sketch.js
+++ p5play/sketch.js
@@ -4,59 +4,47 @@
  */
 
 // Learn more about p5play here -> https://p5play.org/learn
+let chairPic;
+let tablePic;
+let chair;
+let table;
 
-let instructions = 'Tap to create a new sprite, drag to throw it!';
-let box;
+function preload() {
+  chairPic = loadImage("yellowchair.png");
+  tablePic = loadImage("sidetablecopy.png");
+}
 
 function setup() {
-  // creates a canvas that fills the screen
-  createCanvas(400, 400)
+  createCanvas(1500, 800)
+  world.gravity.y = 10
+  chair = new Sprite();
+	chair.scale = 1/4;
+	chair.image = 'yellowchair.png';
+ chair.image.offset.y = 6;
 }
 
 function draw() {
- // clear(); // try removing this line and see what happens!
-
-  textSize(18);
-  textAlign(CENTER);
-  fill(200);
-  text(instructions, canvas.w / 2, canvas.h / 2);
-
+  clear();
+  chair.debug = mouse.pressing();
+  
   if (mouse.presses()) {
-    // by default, sprites collide with other sprites
-    box = new Sprite(mouse.x, mouse.y, 30, 30);
+    table = new Sprite(mouse.x, mouse.y);
+    table.image = "sidetablecopy.png";
+    table.scale = 1/10;
+    table.image.offset.y = -30;
+    table.image.offset.x = 10;
   }
+
   
   if (mouse.dragging()) {
-    box.moveTowards(mouse); // throw the box!
+    table.moveTowards(mouse);
   }
-  
-  // if the user didn't throw the box,
-  // then give it a random speed and direction
+
   if (mouse.released() && !mouse.dragged()) {
-    box.speed = random(0, 5);
-    box.direction = random(0, 360);
+    table.speed = random(0, 5);
+    table.direction = random(0, 360);
   }
 
   // by default, all sprites are drawn by p5play
   // after the end of the draw function
 }
-
-// WARNING! Using the p5.js web editor is not recommended
-// due to these long-standing issues that they won't fix.
-
-// 1. When console.log is used to print a number on
-//    every frame, it causes drops in frame rate.
-//    This makes the p5.js web editor unusable
-//    for play testing certain kinds of games.
-
-// 2. console.log doesn't print out the correct
-//    values of arrays before and after they're
-//    changed.
-
-// 3. The sequence mode of loadAnimation doesn't
-//    work unless you use backticks for the name
-//    of the image file.
-
-// p5play is not affiliated with the Processing Foundation.
-// I can't persuade them to fix these critical issues which
-// make the p5.js web editor inadequate for p5play users.
