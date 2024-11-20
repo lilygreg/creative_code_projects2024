8971812e7734111c9bcc8a9a1e7da3e75b267a6a
lilygreg <lilygreg334@gmail.com>
2024-11-20 11:14:21 -0600

    b

--- portal_puzzle/sketch.js
+++ portal_puzzle/sketch.js
@@ -1,7 +1,28 @@
+//background
 let castle;
+
+//scene 1 items
+let raccoonImg; 
+let cupcakeImg;
+let keyImg;
+let hammerImg;
+let ticketImg;
+
+let raccoon;
+let cupcake;
+let key;
+let hammer;
+let ticket;
+
 
 function preload(){
     castle = loadImage("castlecode.jpg");
+// scene 1 items
+raccoonImg = loadImage("raccoon.png");
+cupcakeImg = loadImage("cupcake.png");
+keyImg = loadImage("key.png");
+hammerImg = loadImage("hammer.png");
+ticketImg = loadImage("planeticket.png");
 }
 
 function setup(){
